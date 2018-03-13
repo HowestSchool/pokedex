ccfa4d4602bb781c9f1268d0f7ebb74acb10c0d2
Jorne <jornesuwie@gmail.com>
2018-03-13 15:03:30 +0100

    Autocomplete in searchbar js + input en datalist tag in html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,18 @@
-"use strict"
+"use strict";
 
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('This loads, hurray!');
+    generatedatalist();
 
+}
+
+function generatedatalist() {
+    let datalist = document.querySelector("#names").innerHTML;
+    let pokemonlijst = [] /*api Fetch*/;
+    for(let i = 0;i<pokemonlijst.length;i++){
+        let pokemonnaam = pokemonlijst[i];
+        datalist += `<option value=${pokemonnaam}`;
+    }
 }
