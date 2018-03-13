b471b08d0d11107c79049174bd73069acbb51de8
Jorne <jornesuwie@gmail.com>
2018-03-13 15:46:40 +0100

    css afbeelding merge conflict fix autcomplete javascript fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', init);
-
+let pokemonlijst = [] /*api Fetch*/;
 function init() {
     console.log('This loads, hurray!');
     generatedatalist();
@@ -10,7 +10,6 @@ function init() {
 
 function generatedatalist() {
     let datalist = document.querySelector("#names").innerHTML;
-    let pokemonlijst = [] /*api Fetch*/;
     for(let i = 0;i<pokemonlijst.length;i++){
         let pokemonnaam = pokemonlijst[i];
         datalist += `<option value=${pokemonnaam}`;
